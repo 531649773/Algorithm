bb5dc2a40533d08afb963fe8429372181423de88
Ziyan <ziyan.frank.zhu@gmail.com>
2016-10-20 22:54:16 -0700

    scan from the middle to the beginning

--- Q214/Shortest_Palindrome.py
+++ Q214/Shortest_Palindrome.py
@@ -17,32 +17,33 @@ def shortest_palindromic(s):
 	S = '#' + '#'.join(s) + '#'
 
 	P = [0] * len(S)
-	MR, pos, Maxlen = 0, 0, 0
-	for i in range(0,len(S)//2+1) :
+	ML, pos, Maxlen = 0, 0, 0
+	for i in range(len(S)//2, 0 ,-1) :
 
-		if i < MR :
-			P[i] = min(P[2*pos - i], 2*(MR - i)+1)
+		if i > ML :
+			P[i] = min(P[2*pos - i], 2*(i - ML)+1)
 		else :
 			P[i] = 1
 
 		if S[2*i-1] == S[1] :
 			while i-P[i]//2 >0 and i+P[i]//2 < len(S)-1 and S[i-P[i]//2] == S[i+P[i]//2]:
 				P[i] += 2
+			if i - P[i]//2 <= 1 :
+				# if we have the LPS starting from the first char of s, then break
+				temp_len = i + P[i]//2 +1
+				break
 
-		if P[i]//2 + i -1 > MR :
-			MR = P[i]//2 + i - 1
+		if i - P[i]//2  < ML :
+			ML = i - P[i]//2 
 			pos = i
 
 		if P[i] > Maxlen :
 			Maxlen = P[i]
-			if i - P[i]//2 == 0 :
-				#temp_center = i
-				temp_len = Maxlen
 				
 	#Maxsubstr = S[temp_center-temp_len//2 +1 : temp_center+ temp_len//2 : 2]
 	res = S[temp_len:: 2]
 	res = res[::-1]
-	return res
+	return res + s
 
 
 
